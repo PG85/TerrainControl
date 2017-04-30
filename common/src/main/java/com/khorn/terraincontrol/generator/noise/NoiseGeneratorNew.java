6ef41107abee46671e1e48c7f5487d36bdd49a97
Jordan <jordan.me2@gmail.com>
2017-04-30 20:56:08 +0100

    Fixed noise artifacts

--- common/src/main/java/com/khorn/terraincontrol/generator/noise/NoiseGeneratorNew.java
+++ common/src/main/java/com/khorn/terraincontrol/generator/noise/NoiseGeneratorNew.java
@@ -4,18 +4,18 @@ import java.util.Random;
 
 public class NoiseGeneratorNew {
 
-    private static final class Double2 {
-        public final double x, y;
+    private static final class Float2 {
+        public final float x, y;
 
-        public Double2(double x, double y) {
+        public Float2(float x, float y) {
             this.x = x;
             this.y = y;
         }
     }
 
-    private static final Double2[] GRAD_2D = {
-        new Double2(-1, -1), new Double2(1, -1), new Double2(-1, 1), new Double2(1, 1),
-        new Double2(0, -1), new Double2(-1, 0), new Double2(0, 1), new Double2(1, 0),
+    private static final Float2[] GRAD_2D = {
+        new Float2(-1, -1), new Float2(1, -1), new Float2(-1, 1), new Float2(1, 1),
+        new Float2(0, -1), new Float2(-1, 0), new Float2(0, 1), new Float2(1, 0),
     };
 
     private int seed;
@@ -29,7 +29,7 @@ public class NoiseGeneratorNew {
         seed = (int)random.nextLong();
     }
 
-    private static int FastFloor(double f) {
+    private static int fastFloor(float f) {
         return (f >= 0 ? (int) f : (int) f - 1);
     }
 
@@ -39,7 +39,7 @@ public class NoiseGeneratorNew {
     //private final static int Z_PRIME = 6971;
     //private final static int W_PRIME = 1013;
 
-    private static double GradCoord2D(int seed, int x, int y, double xd, double yd) {
+    private static float gradCoord2D(int seed, int x, int y, float xd, float yd) {
         int hash = seed;
         hash ^= X_PRIME * x;
         hash ^= Y_PRIME * y;
@@ -47,27 +47,29 @@ public class NoiseGeneratorNew {
         hash = hash * hash * hash * 60493;
         hash = (hash >> 13) ^ hash;
 
-        Double2 g = GRAD_2D[hash & 7];
+        Float2 g = GRAD_2D[hash & 7];
 
         return xd * g.x + yd * g.y;
     }
 
-    private final static double SQRT3 = 1.7320508075f;
-    private final static double F2 = 0.5f * (SQRT3 - 1.0f);
-    private final static double G2 = (3.0f - SQRT3) / 6.0f;
+    private final static float SQRT3 = 1.7320508075f;
+    private final static float F2 = 0.5f * (SQRT3 - 1.0f);
+    private final static float G2 = (3.0f - SQRT3) / 6.0f;
 
     public double a(double x, double y) {
+        float xf = (float)x;
+        float yf = (float)y;
 
-        double f = (x + y) * F2;
-        int i = FastFloor(x + f);
-        int j = FastFloor(y + f);
+        float f = (xf + yf) * F2;
+        int i = fastFloor(xf + f);
+        int j = fastFloor(yf + f);
 
-        double t = (i + j) * G2;
-        double X0 = i - t;
-        double Y0 = j - t;
+        float t = (i + j) * G2;
+        float X0 = i - t;
+        float Y0 = j - t;
 
-        double x0 = x - X0;
-        double y0 = y - Y0;
+        float x0 = xf - X0;
+        float y0 = yf - Y0;
 
         int i1, j1;
         if (x0 > y0) {
@@ -78,32 +80,32 @@ public class NoiseGeneratorNew {
             j1 = 1;
         }
 
-        double x1 = x0 - i1 + G2;
-        double y1 = y0 - j1 + G2;
-        double y2 = y0 - (2 * G2);
-        double x2 = x0 - (2 * G2);
+        float x1 = x0 - i1 + G2;
+        float y1 = y0 - j1 + G2;
+        float y2 = y0 - (2 * G2);
+        float x2 = x0 - (2 * G2);
 
-        double n0, n1, n2;
+        float n0, n1, n2;
 
-        t = 0.5 - x0 * x0 - y0 * y0;
+        t = (float) 0.5 - x0 * x0 - y0 * y0;
         if (t < 0) n0 = 0;
         else {
             t *= t;
-            n0 = t * t * GradCoord2D(seed, i, j, x0, y0);
+            n0 = t * t * gradCoord2D(seed, i, j, x0, y0);
         }
 
-        t = 0.5 - x1 * x1 - y1 * y1;
+        t = (float) 0.5 - x1 * x1 - y1 * y1;
         if (t < 0) n1 = 0;
         else {
             t *= t;
-            n1 = t * t * GradCoord2D(seed, i + i1, j + j1, x1, y1);
+            n1 = t * t * gradCoord2D(seed, i + i1, j + j1, x1, y1);
         }
 
-        t = 0.5 - x2 * x2 - y2 * y2;
+        t = (float) 0.5 - x2 * x2 - y2 * y2;
         if (t < 0) n2 = 0;
         else {
             t *= t;
-            n2 = t * t * GradCoord2D(seed, i + 1, j + 1, x2, y2);
+            n2 = t * t * gradCoord2D(seed, i + 1, j + 1, x2, y2);
         }
 
         return 70f * (n0 + n1 + n2);
@@ -113,21 +115,21 @@ public class NoiseGeneratorNew {
         int index = 0;
 
         for (int iy = 0; iy < ySize; ++iy) {
-            double yf = (yStart + iy) * yScale;
+            float yf = (float)((yStart + iy) * yScale);
 
             for (int ix = 0; ix < xSize; ++ix) {
-                double xf = (xStart + ix) * xScale;
+                float xf = (float)((xStart + ix) * xScale);
 
-                double f = (xf + yf) * F2;
-                int i = FastFloor(xf + f);
-                int j = FastFloor(yf + f);
+                float f = (xf + yf) * F2;
+                int i = fastFloor(xf + f);
+                int j = fastFloor(yf + f);
 
-                double t = (i + j) * G2;
-                double X0 = i - t;
-                double Y0 = j - t;
+                float t = (i + j) * G2;
+                float X0 = i - t;
+                float Y0 = j - t;
 
-                double x0 = xf - X0;
-                double y0 = yf - Y0;
+                float x0 = xf - X0;
+                float y0 = yf - Y0;
 
                 int i1, j1;
                 if (x0 > y0) {
@@ -138,32 +140,32 @@ public class NoiseGeneratorNew {
                     j1 = 1;
                 }
 
-                double x1 = x0 - i1 + G2;
-                double y1 = y0 - j1 + G2;
-                double y2 = y0 - (2 * G2);
-                double x2 = x0 - (2 * G2);
+                float x1 = x0 - i1 + G2;
+                float y1 = y0 - j1 + G2;
+                float y2 = y0 - (2 * G2);
+                float x2 = x0 - (2 * G2);
 
-                double n0, n1, n2;
+                float n0, n1, n2;
 
-                t = 0.5 - x0 * x0 - y0 * y0;
+                t = (float) 0.5 - x0 * x0 - y0 * y0;
                 if (t < 0) n0 = 0;
                 else {
                     t *= t;
-                    n0 = t * t * GradCoord2D(seed, i, j, x0, y0);
+                    n0 = t * t * gradCoord2D(seed, i, j, x0, y0);
                 }
 
-                t = 0.5 - x1 * x1 - y1 * y1;
+                t = (float) 0.5 - x1 * x1 - y1 * y1;
                 if (t < 0) n1 = 0;
                 else {
                     t *= t;
-                    n1 = t * t * GradCoord2D(seed, i + i1, j + j1, x1, y1);
+                    n1 = t * t * gradCoord2D(seed, i + i1, j + j1, x1, y1);
                 }
 
-                t = 0.5 - x2 * x2 - y2 * y2;
+                t = (float) 0.5 - x2 * x2 - y2 * y2;
                 if (t < 0) n2 = 0;
                 else {
                     t *= t;
-                    n2 = t * t * GradCoord2D(seed, i + 1, j + 1, x2, y2);
+                    n2 = t * t * gradCoord2D(seed, i + 1, j + 1, x2, y2);
                 }
 
                 doubleArray[index++] += 70.0D * (n0 + n1 + n2) * noiseScale;
--- common/src/main/java/com/khorn/terraincontrol/generator/noise/NoiseGeneratorPerlin.java
+++ common/src/main/java/com/khorn/terraincontrol/generator/noise/NoiseGeneratorPerlin.java
@@ -4,35 +4,35 @@ import java.util.Random;
 
 public class NoiseGeneratorPerlin
 {
-    private static final class Double2 {
-        public final double x, y;
+    private static final class Float2 {
+        public final float x, y;
 
-        public Double2(double x, double y) {
+        public Float2(float x, float y) {
             this.x = x;
             this.y = y;
         }
     }
 
-    private static final class Double3 {
-        public final double x, y, z;
+    private static final class Float3 {
+        public final float x, y, z;
 
-        public Double3(double x, double y, double z) {
+        public Float3(float x, float y, float z) {
             this.x = x;
             this.y = y;
             this.z = z;
         }
     }
 
-    private static final Double2[] GRAD_2D = {
-        new Double2(-1, -1), new Double2(1, -1), new Double2(-1, 1), new Double2(1, 1),
-        new Double2(0, -1), new Double2(-1, 0), new Double2(0, 1), new Double2(1, 0),
+    private static final Float2[] GRAD_2D = {
+        new Float2(-1, -1), new Float2(1, -1), new Float2(-1, 1), new Float2(1, 1),
+        new Float2(0, -1), new Float2(-1, 0), new Float2(0, 1), new Float2(1, 0),
     };
 
-    private static final Double3[] GRAD_3D = {
-        new Double3(1, 1, 0), new Double3(-1, 1, 0), new Double3(1, -1, 0), new Double3(-1, -1, 0),
-        new Double3(1, 0, 1), new Double3(-1, 0, 1), new Double3(1, 0, -1), new Double3(-1, 0, -1),
-        new Double3(0, 1, 1), new Double3(0, -1, 1), new Double3(0, 1, -1), new Double3(0, -1, -1),
-        new Double3(1, 1, 0), new Double3(0, -1, 1), new Double3(-1, 1, 0), new Double3(0, -1, -1),
+    private static final Float3[] GRAD_3D = {
+        new Float3(1, 1, 0), new Float3(-1, 1, 0), new Float3(1, -1, 0), new Float3(-1, -1, 0),
+        new Float3(1, 0, 1), new Float3(-1, 0, 1), new Float3(1, 0, -1), new Float3(-1, 0, -1),
+        new Float3(0, 1, 1), new Float3(0, -1, 1), new Float3(0, 1, -1), new Float3(0, -1, -1),
+        new Float3(1, 1, 0), new Float3(0, -1, 1), new Float3(-1, 1, 0), new Float3(0, -1, -1),
     };
 
     private int seed;
@@ -42,13 +42,13 @@ public class NoiseGeneratorPerlin
         seed = (int)random.nextLong();
     }
 
-    private static double Lerp(double a, double b, double t) {
+    private static float lerp(float a, float b, float t) {
         return a + t * (b - a);
     }
-    private static int FastFloor(double f) {
+    private static int fastFloor(float f) {
         return (f >= 0 ? (int) f : (int) f - 1);
     }
-    private static double InterpQuinticFunc(double t) {
+    private static float interpQuinticFunc(float t) {
         return t * t * t * (t * (t * 6 - 15) + 10);
     }
 
@@ -58,7 +58,7 @@ public class NoiseGeneratorPerlin
     private final static int Z_PRIME = 6971;
     //private final static int W_PRIME = 1013;
 
-    private static double GradCoord2D(int seed, int x, int y, double xd, double yd) {
+    private Float2 gradCoord2D(int x, int y) {
         int hash = seed;
         hash ^= X_PRIME * x;
         hash ^= Y_PRIME * y;
@@ -66,12 +66,15 @@ public class NoiseGeneratorPerlin
         hash = hash * hash * hash * 60493;
         hash = (hash >> 13) ^ hash;
 
-        Double2 g = GRAD_2D[hash & 7];
-
-        return xd * g.x + yd * g.y;
+        return GRAD_2D[hash & 7];
     }
 
-    private static double GradCoord3D(int seed, int x, int y, int z, double xd, double yd, double zd) {
+    private static float gradDot(Float2 g, float x, float y)
+    {
+        return g.x * x + g.y * y;
+    }
+
+    private Float3 gradCoord3D(int x, int y, int z) {
         int hash = seed;
         hash ^= X_PRIME * x;
         hash ^= Y_PRIME * y;
@@ -80,9 +83,12 @@ public class NoiseGeneratorPerlin
         hash = hash * hash * hash * 60493;
         hash = (hash >> 13) ^ hash;
 
-        Double3 g = GRAD_3D[hash & 15];
+        return GRAD_3D[hash & 15];
+    }
 
-        return xd * g.x + yd * g.y + zd * g.z;
+    private static float gradDot(Float3 g, float x, float y, float z)
+    {
+        return g.x * x + g.y * y + g.z * z;
     }
 
     public void populateNoiseArray3D(double NoiseArray[], double xOffset, double yOffset, double zOffset, int xSize, int ySize, int zSize, double xScale, double yScale, double zScale, double noiseScale)
@@ -91,50 +97,64 @@ public class NoiseGeneratorPerlin
         double invNoiseScale = 1.0D / noiseScale;
 
         int y0Last = 0;
-        double xf00, xf10, xf01, xf11;
-        double zf0 = 0;
-        double zf1 = 0;
+        Float3 g000 = null;
+        Float3 g001 = null;
+        Float3 g010 = null;
+        Float3 g011 = null;
+        Float3 g100 = null;
+        Float3 g101 = null;
+        Float3 g110 = null;
+        Float3 g111 = null;
 
         for (int ix = 0; ix < xSize; ix++)
         {
-            double x = xOffset + ix * xScale;
-            int x0 = FastFloor(x);
+            float x = (float)(xOffset + ix * xScale);
+            int x0 = fastFloor(x);
             int x1 = x0 + 1;
-            double xd0 = x - x0;
-            double xd1 = xd0 - 1;
-            double xs = InterpQuinticFunc(x - x0);
+            float xd0 = x - x0;
+            float xd1 = xd0 - 1;
+            float xs = interpQuinticFunc(x - x0);
 
             for (int iz = 0; iz < zSize; iz++)
             {
-                double z = zOffset + iz * zScale;
-                int z0 = FastFloor(z);
+                float z = (float)(zOffset + iz * zScale);
+                int z0 = fastFloor(z);
                 int z1 = z0 + 1;
-                double zd0 = z - z0;
-                double zd1 = zd0 - 1;
-                double zs = InterpQuinticFunc(z - z0);
+                float zd0 = z - z0;
+                float zd1 = zd0 - 1;
+                float zs = interpQuinticFunc(z - z0);
 
                 for (int iy = 0; iy < ySize; iy++)
                 {
-                    double y = yOffset + iy * yScale;
-                    int y0 = FastFloor(y);
-                    double ys = InterpQuinticFunc(y - y0);
+                    float y = (float)(yOffset + iy * yScale);
+                    int y0 = fastFloor(y);
+                    float ys = interpQuinticFunc(y - y0);
+                    float yd0 = y - y0;
+                    float yd1 = yd0 - 1;
 
                     if (iy == 0 || y0 != y0Last) {
-                        y0Last = y0;
+                        y0Last = z0;
                         int y1 = y0 + 1;
-                        double yd0 = y - y0;
-                        double yd1 = yd0 - 1;
 
-                        xf00 = Lerp(GradCoord3D(seed, x0, y0, z0, xd0, yd0, zd0), GradCoord3D(seed, x1, y0, z0, xd1, yd0, zd0), xs);
-                        xf01 = Lerp(GradCoord3D(seed, x0, y0, z1, xd0, yd0, zd1), GradCoord3D(seed, x1, y0, z1, xd1, yd0, zd1), xs);
-                        xf10 = Lerp(GradCoord3D(seed, x0, y1, z0, xd0, yd1, zd0), GradCoord3D(seed, x1, y1, z0, xd1, yd1, zd0), xs);
-                        xf11 = Lerp(GradCoord3D(seed, x0, y1, z1, xd0, yd1, zd1), GradCoord3D(seed, x1, y1, z1, xd1, yd1, zd1), xs);
-
-                        zf0 = Lerp(xf00, xf01, zs);
-                        zf1 = Lerp(xf10, xf11, zs);
+                        g000 = gradCoord3D(x0, y0, z0);
+                        g001 = gradCoord3D(x0, y0, z1);
+                        g010 = gradCoord3D(x0, y1, z0);
+                        g011 = gradCoord3D(x0, y1, z1);
+                        g100 = gradCoord3D(x1, y0, z0);
+                        g101 = gradCoord3D(x1, y0, z1);
+                        g110 = gradCoord3D(x1, y1, z0);
+                        g111 = gradCoord3D(x1, y1, z1);
                     }
 
-                    NoiseArray[index++] += Lerp(zf0, zf1, ys) * invNoiseScale;
+                    float xf00 = lerp(gradDot(g000, xd0, yd0, zd0), gradDot(g100, xd1, yd0, zd0), xs);
+                    float xf01 = lerp(gradDot(g001, xd0, yd0, zd1), gradDot(g101, xd1, yd0, zd1), xs);
+                    float xf10 = lerp(gradDot(g010, xd0, yd1, zd0), gradDot(g110, xd1, yd1, zd0), xs);
+                    float xf11 = lerp(gradDot(g011, xd0, yd1, zd1), gradDot(g111, xd1, yd1, zd1), xs);
+
+                    float zf0 = lerp(xf00, xf01, zs);
+                    float zf1 = lerp(xf10, xf11, zs);
+
+                    NoiseArray[index++] += lerp(zf0, zf1, ys) * invNoiseScale;
                 }
             }
         }
@@ -146,36 +166,41 @@ public class NoiseGeneratorPerlin
         double invNoiseScale = 1.0D / noiseScale;
 
         int z0Last = 0;
-        double xf0 = 0;
-        double xf1 = 0;
+        Float2 g00 = null;
+        Float2 g01 = null;
+        Float2 g10 = null;
+        Float2 g11 = null;
 
         for (int ix = 0; ix < xSize; ix++)
         {
-            double x = xOffset + ix * xScale;
-            int x0 = FastFloor(x);
+            float x = (float)(xOffset + ix * xScale);
+            int x0 = fastFloor(x);
             int x1 = x0 + 1;
-            double xd0 = x - x0;
-            double xd1 = xd0 - 1;
-            double xs = InterpQuinticFunc(x - x0);
+            float xd0 = x - x0;
+            float xd1 = xd0 - 1;
+            float xs = interpQuinticFunc(x - x0);
 
-            for (int iz = 0; iz < zSize; iz++)
-            {
-                double z = zOffset + iz * zScale;
-                int z0 = FastFloor(z);
-                double zs = InterpQuinticFunc(z - z0);
+            for (int iz = 0; iz < zSize; iz++) {
+                float z = (float) (zOffset + iz * zScale);
+                int z0 = fastFloor(z);
+                float zs = interpQuinticFunc(z - z0);
+                float zd0 = z - z0;
+                float zd1 = zd0 - 1;
 
-                if (iz == 0 || z0 != z0Last)
-                {
+                if (iz == 0 || z0 != z0Last){
                     z0Last = z0;
                     int z1 = z0 + 1;
-                    double zd0 = z - z0;
-                    double zd1 = zd0 - 1;
 
-                    xf0 = Lerp(GradCoord2D(seed, x0, z0, xd0, zd0), GradCoord2D(seed, x1, z0, xd1, zd0), xs);
-                    xf1 = Lerp(GradCoord2D(seed, x0, z1, xd0, zd1), GradCoord2D(seed, x1, z1, xd1, zd1), xs);
+                    g00 = gradCoord2D(x0, z0);
+                    g01 = gradCoord2D(x0, z1);
+                    g10 = gradCoord2D(x1, z0);
+                    g11 = gradCoord2D(x1, z1);
                 }
 
-                NoiseArray[index++] += Lerp(xf0, xf1, zs) * invNoiseScale;
+                float xf0 = lerp(gradDot(g00, xd0, zd0), gradDot(g10, xd1, zd0), xs);
+                float xf1 = lerp(gradDot(g01, xd0, zd1), gradDot(g11, xd1, zd1), xs);
+
+                NoiseArray[index++] += lerp(xf0, xf1, zs) * invNoiseScale;
             }
         }
 
