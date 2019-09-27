f34f9f04de5e15fcd4a1d577a695e0e35f1de279
coll1234567 <joshl5324@gmail.com>
2019-09-27 12:04:47 -0400

    Fixed a crash with BO4s.

--- common/src/main/java/com/pg85/otg/customobjects/bo3/BO3.java
+++ common/src/main/java/com/pg85/otg/customobjects/bo3/BO3.java
@@ -493,6 +493,6 @@ public class BO3 implements StructuredCustomObject
 
 	@Override
 	public boolean loadChecks() {
-		return this.settings.parseModChecks();
+		return settings == null ? false : this.settings.parseModChecks();
 	}
 }
